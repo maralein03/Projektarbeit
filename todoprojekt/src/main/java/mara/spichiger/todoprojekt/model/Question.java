9b34cdf2f91c9e90ffbda52b03e0a519d3f91fa2
maralein03 <mara.spichiger@bbzbl-it.com>
2026-05-08 08:21:19 +0200

    fix: model need description and limit

--- todoprojekt/src/main/java/mara/spichiger/todoprojekt/model/Question.java
+++ todoprojekt/src/main/java/mara/spichiger/todoprojekt/model/Question.java
@@ -2,6 +2,7 @@ package mara.spichiger.todoprojekt.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -10,9 +11,59 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String text;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String author;
+
+    private LocalDateTime timestamp;
 
     @ManyToOne
     @JoinColumn(name = "todo_id")
     private Todo todo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Todo getTodo() {
+        return todo;
+    }
+
+    public void setTodo(Todo todo) {
+        this.todo = todo;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        timestamp = LocalDateTime.now();
+    }
 }
